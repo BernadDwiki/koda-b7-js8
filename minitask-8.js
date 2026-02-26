e6f7216368f98f274ccd50d24c33e827a0de44a3
BernadDwiki <125329942+BernadDwiki@users.noreply.github.com>
2026-02-26 16:32:51 +0700

    Add files via upload

---
+++ minitask-8.js
@@ -0,0 +1,37 @@
+const movie = {
+    id : 123,
+    title : "okela",
+    image : "ikan",
+    genre : ["serem", "lucu", "gokil"],
+    cast : [{
+        name : "Jono",
+        role : "pemain utama"
+    }, {
+        name : "Kasino",
+        role : "sampingan"
+    }],
+    getFilm: function(){
+        return `Judul : ${this.title} 
+Gambar : ${this.image} 
+Genre : ${this.genre}`
+    },
+
+    getCast: function(){
+        return `Cast : ${this.cast[0].name}, ${this.cast[1].name}`
+    },
+
+    setTitle: function(newTitle){
+        this.title = newTitle
+    },
+
+    setImage: function(newImage){
+        this.image = newImage
+    }
+}
+
+console.log(movie.getFilm())
+console.log(movie.getCast())
+movie.setTitle("Uwaw")
+console.log("Judul Film : "+movie.title)
+movie.setImage("Badak")
+console.log("Gambar : "+movie.image)
